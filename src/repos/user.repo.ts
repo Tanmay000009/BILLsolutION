ccd603b7f510258f52a13fbccbce7682d86be152
tanmay000009 <tanmayvyas009@gmail.com>
2023-08-22 15:00:01 +0530

    add user repository

---
+++ src/repos/user.repo.ts
@@ -0,0 +1,35 @@
+import { CreateUserDto } from '../dtos/user.dto';
+import { User } from '../models/User.model';
+import { ds } from '../utils/datasource';
+
+const getByEmail = async (email: string) => {
+  return await ds.getRepository(User).findOne({
+    where: { email }
+  });
+};
+
+const createUser = async (user: CreateUserDto, firebaseId: string) => {
+  return await ds.getRepository(User).save({
+    ...user,
+    firebaseId
+  });
+};
+
+const createAdmin = async (user: CreateUserDto, firebaseId: string) => {
+  return await ds.getRepository(User).save({
+    ...user,
+    firebaseId,
+    isAdmin: true
+  });
+};
+
+const updateUser = async (user: User) => {
+  return await ds.getRepository(User).save(user);
+};
+
+export const userRepo = {
+  getByEmail,
+  createUser,
+  createAdmin,
+  updateUser
+};
