a3a4a68e681e89c3cbf2c44938b8bc3c40a15989
tanmay000009 <tanmayvyas009@gmail.com>
2023-08-22 21:25:04 +0530

    add uuid validator

--- src/dtos/common.dto.ts
+++ src/dtos/common.dto.ts
@@ -1,8 +1,15 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class EmailValidationDto {
   @IsEmail()
   @IsString()
   @IsNotEmpty()
   email: string;
+}
+
+export class UUIDValidationDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsUUID()
+  id: string;
 }
