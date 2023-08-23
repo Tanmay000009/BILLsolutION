fbe13faa7b6e530e839ca83564b08f732fe999a8
tanmay000009 <tanmayvyas009@gmail.com>
2023-08-23 19:32:14 +0530

    handle checkJWT token expiration

--- src/middleware/checkJWT.ts
+++ src/middleware/checkJWT.ts
@@ -15,25 +15,32 @@ export const checkJWT = async (
     });
   }
 
-  const dbUser = await admin.auth().verifyIdToken(token);
+  try {
+    const dbUser = await admin.auth().verifyIdToken(token);
 
-  if (!dbUser) {
+    if (!dbUser) {
+      return res.status(401).json({
+        status: false,
+        message: 'Invalid token'
+      });
+    }
+
+    const user = await userRepo.getByEmail(dbUser.email as string);
+
+    if (!user) {
+      return res.status(401).json({
+        status: false,
+        message: 'User not found'
+      });
+    }
+
+    req.user = user;
+  } catch (error) {
     return res.status(401).json({
       status: false,
       message: 'Invalid token'
     });
   }
-
-  const user = await userRepo.getByEmail(dbUser.email as string);
-
-  if (!user) {
-    return res.status(401).json({
-      status: false,
-      message: 'User not found'
-    });
-  }
-
-  req.user = user;
 
   return next();
 };
