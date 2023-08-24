db83a4012165bcaaba83d748286b5cbb77c0d051
tanmay000009 <tanmayvyas009@gmail.com>
2023-08-24 15:07:47 +0530

    fix user.routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -7,7 +7,7 @@ import { userController } from '../controllers/user.controller';
 const router = Router();
 
 router.get('/', checkJWT, userController.getMe);
-
 router.put('/', checkJWT, userController.updateUser);
+router.put('/:email/admin', checkJWT, userController.makeAdmin);
 
-export default router.post('/:email/admin', checkJWT, userController.makeAdmin);
+export default router;
