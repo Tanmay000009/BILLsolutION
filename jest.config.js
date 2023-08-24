c29f1fbc549b4ae326bee22078f8d237706dd823
tanmay000009 <tanmayvyas009@gmail.com>
2023-08-24 23:07:12 +0530

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }], // Transpiles for the Node.js version you're using
-    '@babel/preset-typescript' // Transpiles TypeScript
-  ],
+  preset: 'ts-jest', // Informs Jest that TypeScript is being used
   testEnvironment: 'node', // Specifies Node.js as the testing environment
   testMatch: ['**/*.test.ts'], // Matches test files ending with '.test.ts'
   verbose: true, // Provides detailed output during test runs
